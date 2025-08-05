32195ad2292f1fd449061f1c39c0f253c6727bbc
cruzmenezes <cruz.menezes@outlook.com>
2025-08-05 20:24:51 -0300

    usuario

--- aprendendo-spring/src/main/java/com/exampleJavaSpring/ExemploDTO/model1/Usuario.java
+++ aprendendo-spring/src/main/java/com/exampleJavaSpring/ExemploDTO/model1/Usuario.java
@@ -1,5 +1,54 @@
 package com.exampleJavaSpring.ExemploDTO.model1;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
+
+@Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String email;
+    private String senha;
+
+    //getters e setters
+
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getSenha() {
+        return senha;
+    }
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
+
+   
+
+    
+    
     
 }
