9eec1b95553787920462c23f5180124dd8acec32
cruzmenezes <cruz.menezes@outlook.com>
2025-08-07 19:59:08 -0300

    usuarioDTO

--- aprendendo-spring/src/main/java/com/exampleJavaSpring/ExemploDTO/dto/UsuarioDTO.java
+++ aprendendo-spring/src/main/java/com/exampleJavaSpring/ExemploDTO/dto/UsuarioDTO.java
@@ -1,5 +1,19 @@
 package com.exampleJavaSpring.ExemploDTO.dto;
 
 public class UsuarioDTO {
+
+    private nome;
+    private email;
+
+    public UsuarioDTO(){ }
+
+    public UsuarioDTO(String nome, String email){
+        this.nome;
+        this.email;
+    }
+
+
+
+
     
 }
